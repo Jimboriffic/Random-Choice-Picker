a4701951ad9a37aee20eff271c915b39ad553fef
Jimboriffic <james.sliwa@gmail.com>
2023-11-18 19:53:56 +0100

    added functions

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ textArea.focus()
 
 textArea.addEventListener('keydown', (e) => {
     createTags(e.target.value)
+
+    if(e.key === 'Enter')
+        setTimeout(() => {
+        e.target.value = ''
+    }, 10)
+
+    randomSelect()
 })
 
 function createTags(input) {
@@ -21,3 +28,18 @@ function createTags(input) {
         tagsEl.appendChild(tagEl)
     })
 }
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+      const randomTag = pickRandomTag()  
+    }, 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function 
